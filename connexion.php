a3a8b97153cf7bcbc9a0cbe10828f73673f746b1
BySmyle <edouardperezpro@gmail.com>
2023-10-23 17:35:36 +0200

    Fin page connexion + creation page wischord et js

--- connexion.php
+++ connexion.php
@@ -1,8 +1,19 @@
 <?php
 $login = $_POST['login'];
-$mdp = ['mdp'];
-$data = json_decode(file_get_contents('./data/mesdonnees.json', true));
+$mdp = $_POST['mdp'];
+$sel = "WishcordVaRenaitreDeSesCendres";
+$data = json_decode(file_get_contents('./data/mesdonnees.json'), true);
 $count = count($data);
-//test
+$i = 0;
+$mdphash = hash('sha256', $mdp .= $sel);
+while ($i < $count) {
+    if ($data[$i]['login'] == $login && $data[$i]['mdp'] == $mdphash) {
+        header('Location: ./wishcord.php');
+        break;
+    } else {
+        echo 'Log failed';
+        $i++;
+    }
+}
 
 ?>
