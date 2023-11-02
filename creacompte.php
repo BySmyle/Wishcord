d5376e687cd573b869d76d2980b599d5fafc0792
BySmyle <edouardperezpro@gmail.com>
2023-11-02 19:35:11 +0100

    wishcord corrigé

--- creacompte.php
+++ creacompte.php
@@ -1,14 +1,30 @@
 <?php
 $login = $_POST['login'];
 $mdp = $_POST['mdp'];
+$i = 0;
+$found = false;
 $sel = "WishcordVaRenaitreDeSesCendres";
-$data = json_decode(file_get_contents("./data/mesdonnees.json", true));
-$mdphash = hash('sha256', $mdp .= $sel);    
-$tab = array(
-    "login" => $login,
-    "mdp" => $mdphash,
-    "serv" => ''
-);
-$data[] = $tab;
-file_put_contents('./data/mesdonnees.json', json_encode($data));
-?>
+$data = json_decode(file_get_contents("./data/mesdonnees.json"), true);
+$count = count($data);
+while ($i < $count) {
+    if ($login == $data[$i]['login']) {
+        $found = true;
+        break;
+    } else {
+        $i++;
+    }
+}
+if ($found == false) {
+    $mdphash = hash('sha256', $mdp .= $sel);
+    $tab = array(
+        "login" => $login,
+        "mdp" => $mdphash,
+        "serv" => ''
+    );
+    $data[] = $tab;
+    file_put_contents('./data/mesdonnees.json', json_encode($data));
+    header('Location: ./accueil.html');
+}else{
+    header('Location: ./creacompte.html');
+}
+
