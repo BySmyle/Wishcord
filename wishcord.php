99880b6e02c606bb06c7e5b2bbd6dd2c749b0d4b
BySmyle <edouardperezpro@gmail.com>
2023-12-07 14:49:38 +0100

    Revert "commit"
    This reverts commit c148d377660259f4c6c4c2e243c463135d500c81.

--- wishcord.php
+++ wishcord.php
@@ -1,7 +1,3 @@
 <?php
-<<<<<<< Updated upstream
     echo'<link href="./wishcss.css" rel="stylesheet" /><script type="text/javascript" src="javascript.js"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script><div id="tout"><div id="messervetcree"><div id="creajoin"><input type="text" id="serv"><input type="button" id="creajoin" value="Créé / Rejoindre" onclick="creajoin()"></div><div id="messerv"></div></div><div id="matextare"><textarea id="maTA" readonly></textarea></div></div><div id="envoie"><input type="text" id="message"><input type="button" id="envoiemsg" onclick="envoiemessage()" value="Envoie"></div>';
-=======
-    echo'<link href="./wishcss.css" rel="stylesheet" /><script type="text/javascript" src="javascript.js"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script><div id="madiv"><input type="text" id="serv"><input type="button" id="creajoin" value="Créé / Rejoindre" onclick="creajoin()"><textarea id="textarea" disabled></textarea><input type="button" onclick="afficheserv()" value="test"/></div>';
->>>>>>> Stashed changes
 ?>
